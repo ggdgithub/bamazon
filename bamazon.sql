0f8ba3cae3af0782be18f8148cbc313e8f5b34a5
Gabriel de Leon <gabriel.deleon0126@gmail.com>
2019-09-07 23:37:16 -0700

    Made 10 values for DB; theme is Vending Machine; formatted via Alt-Shift-F

--- bamazon.sql
+++ bamazon.sql
@@ -6,9 +6,29 @@ USE products;
 
 CREATE TABLE products
 (
-  item_id INT(5) NOT NULL,
-  product_name VARCHAR(100) NOT NULL,
-  department_name VARCHAR(100) NOT NULL,
-  price DECIMAL(10,2) NOT NULL,
-  stock_quantity INT(20) NOT NULL,
+  item_id INT NOT NULL
+  AUTO_INCREMENT,
+  product_name VARCHAR
+  (100) NOT NULL,
+  department_name VARCHAR
+  (100) NOT NULL,
+  price DECIMAL
+  (10,2) NOT NULL,
+  stock_quantity INT
+  (20) NOT NULL,
+  PRIMARY KEY item_id
 );
+
+  INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+  VALUES
+    ('Coca-Cola', 'Drinks', 1.25, 10),
+    ('Fanta Orange', 'Drinks', 1.25, 14),
+    ('Dasani', 'Drinks', 1.00, 5),
+    ('Diet Coke', 'Drinks', 1.25, 1),
+    ('Trail Mix', 'Snacks', 1.50, 6),
+    ('Cheetos', 'Snacks', 1.50, 15),
+    ('Lays', 'Snacks', 1.50, 14),
+    ('Snickers', 'Candy', 1.25, 6),
+    ('Twix', 'Candy', 1.25, 15),
+    ('Juicy Fruit', 'Gum', 0.75, 8);
